7f30ddf4da9e48f614d08f0ee7323e182fdb5681
Christian Melendez <christian.melendez2652@gmail.com>
2024-10-08 22:14:18 -0500

    Adding module A view controller and navigation controller

---
+++ AudioLabSwift/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  AudioLabSwift
+//
+//  Created by Travis Peck on 10/8/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func ModuleAButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
